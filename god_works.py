3b1f9736f69a28f94ab5475f87778ed8a7656915
phillipsk <kjphillips@suffolk.edu>
2015-02-15 19:52:22 -0500

    edit boiler plate code

--- god_works.py
+++ god_works.py
@@ -119,15 +119,17 @@ app.add_url_rule('/hymns/',
 
 
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
-"""
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     if port == 5000:
         app.debug = True
     app.run(host='0.0.0.0', port=port)
+"""
+
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
